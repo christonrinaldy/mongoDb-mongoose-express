2ec4351a8352b1dd24933bc764506f456d0a267c
christonrinaldy <christonrinaldy.geodesy@gmail.com>
2020-11-16 08:34:26 +0700

    3rd commit

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+require('dotenv').config();
 const express = require('express')
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose');
 const routes = require('./routes/index')
 
-
 const app = express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-
+console.log(process.env.uri)
 //mongoDb on mongoAtlas
-mongoose.connect('mongodb://dbUser:Z0Zytr7Rc5PtHBmp@cluster0-shard-00-00.8ubes.mongodb.net:27017,cluster0-shard-00-01.8ubes.mongodb.net:27017,cluster0-shard-00-02.8ubes.mongodb.net:27017/Users?ssl=true&replicaSet=atlas-230pxs-shard-0&authSource=admin&retryWrites=true&w=majority"', {useNewUrlParser: true, useUnifiedTopology: true},(err) => {
+mongoose.connect(process.env.uri, {useNewUrlParser: true, useUnifiedTopology: true},(err) => {
     if(err) {
         console.log({error: err})
     } else {
